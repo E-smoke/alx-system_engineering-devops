7e35017af995d953655b7f92f4173086329f74cc
Elijah <eachala5@gmail.com>
2024-03-30 04:14:13 -0700

    0004

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,5 +2,5 @@
 file { '~/.ssh/config':
   ensure => file,
   path   => '/etc/ssh/ssh_config',
- content => 'Host 54.224.37.165\nHostName 54.224.37.165\nIdentityFile ~/.ssh/school\nPasswordAuthentication no',
+ content => "Host 54.224.37.165\nHostName 54.224.37.165\nIdentityFile ~/.ssh/school\nPasswordAuthentication no",
 }
